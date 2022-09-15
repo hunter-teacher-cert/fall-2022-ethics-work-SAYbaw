efbe835be0b77452fc6a80c720d3fea3db67bba2
SAYbaw <97255560+SAYbaw@users.noreply.github.com>
2022-09-15 15:49:37 -0400

    Update nim.py

--- py/nim.py
+++ py/nim.py
@@ -1,6 +1,39 @@
 # nim.py
 # Steve Sabaugh
 # CSCI 77800 Fall 2022
-# collaborators: 
+# collaborators: Mike Randazzo (That is the only person I could remember collaborating with on Java NIM during the summer.)
 # consulted: 
 
+import random
+
+stones = 12
+com_num = random.randint(1, min(3, stones))
+print("Lets Play the Game of Nim")
+print(f'There are {stones} stones left')
+
+while stones >= 0:
+
+    user_num = int(input("Pick a number from 1 to 3: "))
+
+    stones -= user_num
+    print("You removed " + str(user_num) + " stones.")
+    print(f'There are {stones} stones left')
+
+    if stones == 0:
+        print("You Won!!")
+        break
+
+    if stones <= 3:
+        com_num = stones
+
+    stones -= com_num
+
+    print("The computer removed " + str(com_num) + " stones")
+    print(f'There are {stones} stones left')
+
+    if stones == 0:
+        print("The Computer Won!!")
+        break
+
+    com_num = random.randint(1, min(3, stones))
+    print()
