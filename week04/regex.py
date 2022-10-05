9cc4f8c675a4b7f61b81e78d6f115501c2a70c3a
SAYbaw <97255560+SAYbaw@users.noreply.github.com>
2022-10-05 18:02:44 -0400

    Update regex.py

--- week04/regex.py
+++ week04/regex.py
@@ -1,3 +1,23 @@
 #Steve Sabaugh
 #regex.py
 #data file to be included mames.txt
+
+import re
+
+
+def find_names(line):
+    pattern = r"(?![A-Z][A-Z]\,)([A-Z][a-zA-Z]+)"
+    result = re.findall(pattern, line)
+
+    return result
+
+f = open("venv/names.txt")
+count = 0
+for line in f.readlines():
+    #print(line)
+    result = find_names(line)
+    if (len(result)>0):
+        print(result)
+        count += 1
+
+print('name count: ', count)
